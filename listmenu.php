d5f8372e638b8555185dee2c3d5a8a7de8b35283
Aldi Prasetyo <127328037+AldiPrasetyoo@users.noreply.github.com>
2024-10-21 14:08:39 +0700

    Update listmenu.php

--- listmenu.php
+++ listmenu.php
@@ -1,1 +1,19 @@
 
+<?php
+$coffee_data = [
+    [
+        "coffee_id" => 1,
+        "coffee_title" => "Caffè Latte",
+        "coffee_detail" => "Rich, full-bodied espresso in steamed milk, lightly topped with foam...",
+        "coffee_thumbnails" => "caffe_latte_thumbnail.png",
+        "coffee_poster" => "caffe_latte_poster.jpg"
+    ],
+    [
+        "coffee_id" => 2,
+        "coffee_title" => "Cappuccino",
+        "coffee_detail" => "Espresso with steamed milk, topped with a deep layer of foam.",
+        "coffee_thumbnails" => "cappuccino_thumbnail.png",
+        "coffee_poster" => "cappuccino_poster.jpg"
+    ],
+];
+?>
